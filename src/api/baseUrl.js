4b6b6b777f4a6bdb79aa615bccfdc58eaf0a9966
Lukasz Banasik <lukbanasik@gmail.com>
2019-03-24 22:57:23 +0100

    First steps of prod build

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,4 +1,17 @@
-export default function getBaseUrl(){
-    const inDevelopment = window.location.hostname === 'localhost';
-    return inDevelopment ? 'http://localhost:8081/' : '/';
-}
+export default function getBaseUrl() {
+   return getQueryStringParameterByName('useMockApi') ? 'http://localhost:8081/' : '/';
+ }
+ function getQueryStringParameterByName(name, url) {
+   console.log('name: ', name);
+   console.log('url: ', url);
+
+   if (!url) url = window.location.href;
+   console.log('url2: ', url);
+   name = name.replace(/[\[\]]/g, "\\$&");
+   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+       results = regex.exec(url);
+       console.log('res: ', results);
+   if (!results) return null;
+   if (!results[2]) return '';
+   return decodeURIComponent(results[2].replace(/\+/g, " "));
+ }
